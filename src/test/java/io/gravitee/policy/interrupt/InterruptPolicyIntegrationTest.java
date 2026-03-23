33e85aefaeb6c8ceb20b280854a27811b75caf62
Michel Barret <michel.barret@graviteesource.com>
2026-03-23 10:42:05 +0100

    test: refactor test

--- src/test/java/io/gravitee/policy/interrupt/InterruptPolicyIntegrationTest.java
+++ src/test/java/io/gravitee/policy/interrupt/InterruptPolicyIntegrationTest.java
@@ -16,16 +16,24 @@
 package io.gravitee.policy.interrupt;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import io.gravitee.apim.gateway.tests.sdk.AbstractPolicyTest;
 import io.gravitee.apim.gateway.tests.sdk.annotations.DeployApi;
 import io.gravitee.apim.gateway.tests.sdk.annotations.GatewayTest;
 import io.gravitee.policy.interrupt.configuration.InterruptPolicyConfiguration;
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.functions.Consumer;
 import io.vertx.core.http.HttpMethod;
+import io.vertx.junit5.VertxTestContext;
+import io.vertx.rxjava3.core.buffer.Buffer;
 import io.vertx.rxjava3.core.http.HttpClient;
-import java.util.concurrent.TimeUnit;
+import io.vertx.rxjava3.core.http.HttpClientRequest;
+import io.vertx.rxjava3.core.http.HttpClientResponse;
+import java.util.function.BiConsumer;
 import lombok.SneakyThrows;
+import org.assertj.core.api.SoftAssertions;
+import org.jspecify.annotations.NonNull;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -41,117 +49,113 @@ class InterruptPolicyIntegrationTest
   @Test
   @DisplayName("Should do interrupt on-request")
   @DeployApi("/apis/interrupt.json")
-  void shouldDoInterruptOnRequest(HttpClient client) {
+  void shouldDoInterruptOnRequest(HttpClient client, VertxTestContext ctx) {
     final var obs = client
       .rxRequest(HttpMethod.GET, "/test")
-      .flatMap(request -> request.rxSend())
-      .flatMapPublisher(response -> {
-        assertThat(response.statusCode()).isEqualTo(500);
-        return response.toFlowable();
-      })
-      .test();
-
-    obs.await(30000, TimeUnit.MILLISECONDS);
-    obs
-      .assertComplete()
-      .assertValue(buffer -> {
-        assertThat(buffer.toString()).isEqualTo("Message in a bottle...");
-        return true;
-      })
-      .assertNoErrors();
-
-    wiremock.verify(0, getRequestedFor(urlPathEqualTo("/endpoint")));
+      .flatMap(HttpClientRequest::rxSend)
+      .flatMapPublisher(InterruptPolicyIntegrationTest::buildResult)
+      .subscribe(
+        asserts(
+          ctx,
+          (softly, result) -> {
+            softly.assertThat(result.statusCode()).isEqualTo(500);
+            softly
+              .assertThat(result.body())
+              .hasToString("Message in a bottle...");
+          },
+          () -> wiremock.verify(0, getRequestedFor(urlPathEqualTo("/endpoint")))
+        ),
+        ctx::failNow
+      );
   }
 
   @SneakyThrows
   @Test
   @DisplayName("Should do interrupt on-request with custom status code")
   @DeployApi("/apis/interrupt-customstatuscode.json")
-  void shouldDoInterruptOnRequestWithCustomStatusCode(HttpClient client) {
-    final var obs = client
+  void shouldDoInterruptOnRequestWithCustomStatusCode(
+    HttpClient client,
+    VertxTestContext ctx
+  ) {
+    client
       .rxRequest(HttpMethod.GET, "/test")
-      .flatMap(request -> request.rxSend())
-      .flatMap(response -> {
-        assertThat(response.statusCode()).isEqualTo(503);
-        assertThat(response.statusMessage()).isEqualTo("Service Unavailable");
-        return response.rxBody();
-      })
-      .toFlowable()
-      .test();
-
-    obs.await(30000, TimeUnit.MILLISECONDS);
-    obs
-      .assertComplete()
-      .assertValue(buffer -> {
-        assertThat(buffer.toString()).isEqualTo("Service unavailable...");
-        return true;
-      })
-      .assertNoErrors();
-
-    wiremock.verify(0, getRequestedFor(urlPathEqualTo("/endpoint")));
+      .flatMap(HttpClientRequest::rxSend)
+      .flatMapPublisher(InterruptPolicyIntegrationTest::buildResult)
+      .subscribe(
+        asserts(
+          ctx,
+          (softly, result) -> {
+            softly.assertThat(result.statusCode()).isEqualTo(503);
+            softly
+              .assertThat(result.body())
+              .hasToString("Service unavailable...");
+          },
+          () -> wiremock.verify(0, getRequestedFor(urlPathEqualTo("/endpoint")))
+        ),
+        ctx::failNow
+      );
   }
 
   @SneakyThrows
   @Test
   @DisplayName("Should do interrupt on-request with response template")
   @DeployApi("/apis/interrupt-responsetemplate.json")
-  void shouldDoInterruptOnRequestWithResponseTemplate(HttpClient client) {
-    final var obs = client
+  void shouldDoInterruptOnRequestWithResponseTemplate(
+    HttpClient client,
+    VertxTestContext ctx
+  ) {
+    client
       .rxRequest(HttpMethod.GET, "/test")
       .flatMap(request -> {
         request.putHeader("my-custom-header", "the anonymous consumer");
         return request.rxSend();
       })
-      .flatMap(response -> {
-        assertThat(response.statusCode()).isEqualTo(400);
-        assertThat(response.statusMessage()).isEqualTo("Bad Request");
-        return response.rxBody();
-      })
-      .toFlowable()
-      .test();
-
-    obs.await(30000, TimeUnit.MILLISECONDS);
-    obs
-      .assertComplete()
-      .assertValue(buffer -> {
-        assertThat(buffer.toString()).isEqualTo(
-          "An other message in a bottle... from the anonymous consumer"
-        );
-        return true;
-      })
-      .assertNoErrors();
-
-    wiremock.verify(0, getRequestedFor(urlPathEqualTo("/endpoint")));
+      .flatMapPublisher(InterruptPolicyIntegrationTest::buildResult)
+      .subscribe(
+        asserts(
+          ctx,
+          (softly, result) -> {
+            softly.assertThat(result.statusCode()).isEqualTo(400);
+            softly
+              .assertThat(result.body())
+              .hasToString(
+                "An other message in a bottle... from the anonymous consumer"
+              );
+          },
+          () -> wiremock.verify(0, getRequestedFor(urlPathEqualTo("/endpoint")))
+        ),
+        ctx::failNow
+      );
   }
 
   @SneakyThrows
   @Test
   @DisplayName("Should do interrupt on-request-content")
   @DeployApi("/apis/interrupt-onrequestcontent.json")
-  void shouldDoInterruptOnRequestContent(HttpClient client) {
+  void shouldDoInterruptOnRequestContent(
+    HttpClient client,
+    VertxTestContext ctx
+  ) {
     wiremock.stubFor(post("/endpoint").willReturn(ok("response from backend")));
 
-    final var obs = client
+    client
       .rxRequest(HttpMethod.POST, "/test")
-      .flatMap(request -> request.rxSend())
-      .flatMap(response -> {
-        assertThat(response.statusCode()).isEqualTo(500);
-        assertThat(response.statusMessage()).isEqualTo("Internal Server Error");
-        return response.rxBody();
-      })
-      .toFlowable()
-      .test();
-
-    obs.await(30000, TimeUnit.MILLISECONDS);
-    obs
-      .assertComplete()
-      .assertValue(buffer -> {
-        assertThat(buffer.toString()).isEqualTo("Message in a bottle...");
-        return true;
-      })
-      .assertNoErrors();
-
-    wiremock.verify(0, postRequestedFor(urlPathEqualTo("/endpoint")));
+      .flatMap(HttpClientRequest::rxSend)
+      .flatMapPublisher(InterruptPolicyIntegrationTest::buildResult)
+      .subscribe(
+        asserts(
+          ctx,
+          (softly, result) -> {
+            softly.assertThat(result.statusCode()).isEqualTo(500);
+            softly
+              .assertThat(result.body())
+              .hasToString("Message in a bottle...");
+          },
+          () ->
+            wiremock.verify(0, postRequestedFor(urlPathEqualTo("/endpoint")))
+        ),
+        ctx::failNow
+      );
   }
 
   @SneakyThrows
@@ -159,128 +163,120 @@ class InterruptPolicyIntegrationTest
   @DisplayName("Should do interrupt on-request-content with response template")
   @DeployApi("/apis/interrupt-onrequestcontent-responsetemplate.json")
   void shouldDoInterruptOnRequestContentWithResponseTemplate(
-    HttpClient client
+    HttpClient client,
+    VertxTestContext ctx
   ) {
     wiremock.stubFor(post("/endpoint").willReturn(ok("response from backend")));
 
-    final var obs = client
+    client
       .rxRequest(HttpMethod.POST, "/test")
       .flatMap(request -> {
         request.putHeader("my-custom-header", "the anonymous consumer");
-        return request.rxSend(
-          io.vertx.rxjava3.core.buffer.Buffer.buffer("request payload")
-        );
+        return request.rxSend(Buffer.buffer("request payload"));
       })
-      .flatMapPublisher(response -> {
-        assertThat(response.statusCode()).isEqualTo(400);
-        assertThat(response.statusMessage()).isEqualTo("Bad Request");
-        return response.toFlowable();
-      })
-      .test();
-
-    obs.await(30000, TimeUnit.MILLISECONDS);
-    obs
-      .assertComplete()
-      .assertValue(buffer -> {
-        assertThat(buffer.toString()).isEqualTo(
-          "An other message in a bottle... from the anonymous consumer"
-        );
-        return true;
-      })
-      .assertNoErrors();
-
-    wiremock.verify(0, postRequestedFor(urlPathEqualTo("/endpoint")));
+      .flatMapPublisher(InterruptPolicyIntegrationTest::buildResult)
+      .subscribe(
+        asserts(
+          ctx,
+          (softly, result) -> {
+            softly.assertThat(result.statusCode()).isEqualTo(400);
+            softly
+              .assertThat(result.body())
+              .hasToString(
+                "An other message in a bottle... from the anonymous consumer"
+              );
+          },
+          () ->
+            wiremock.verify(0, postRequestedFor(urlPathEqualTo("/endpoint")))
+        ),
+        ctx::failNow
+      );
   }
 
   @SneakyThrows
   @Test
   @DisplayName("Should do interrupt on-response")
   @DeployApi("/apis/interrupt-onresponse.json")
-  void shouldDoInterruptOnResponse(HttpClient client) {
-    final var obs = client
+  void shouldDoInterruptOnResponse(HttpClient client, VertxTestContext ctx) {
+    client
       .rxRequest(HttpMethod.GET, "/test")
-      .flatMap(request -> request.rxSend())
-      .flatMap(response -> {
-        assertThat(response.statusCode()).isEqualTo(500);
-        assertThat(response.statusMessage()).isEqualTo("Internal Server Error");
-        return response.rxBody();
-      })
-      .toFlowable()
-      .test();
-
-    obs.await(30000, TimeUnit.MILLISECONDS);
-    obs
-      .assertComplete()
-      .assertValue(buffer -> {
-        assertThat(buffer.toString()).isEqualTo("Message in a bottle...");
-        return true;
-      })
-      .assertNoErrors();
-
-    wiremock.verify(1, getRequestedFor(urlPathEqualTo("/endpoint")));
+      .flatMap(HttpClientRequest::rxSend)
+      .flatMapPublisher(InterruptPolicyIntegrationTest::buildResult)
+      .subscribe(
+        asserts(
+          ctx,
+          (softly, result) -> {
+            softly.assertThat(result.statusCode()).isEqualTo(500);
+            softly
+              .assertThat(result.body())
+              .hasToString("Message in a bottle...");
+          },
+          () -> wiremock.verify(1, getRequestedFor(urlPathEqualTo("/endpoint")))
+        ),
+        ctx::failNow
+      );
   }
 
   @SneakyThrows
   @Test
   @DisplayName("Should do interrupt on-response with response template")
   @DeployApi("/apis/interrupt-onresponse-responsetemplate.json")
-  void shouldDoInterruptOnResponseWithResponseTemplate(HttpClient client) {
-    final var obs = client
+  void shouldDoInterruptOnResponseWithResponseTemplate(
+    HttpClient client,
+    VertxTestContext ctx
+  ) {
+    client
       .rxRequest(HttpMethod.GET, "/test")
       .flatMap(request -> {
         request.putHeader("my-custom-header", "the anonymous consumer");
         return request.rxSend();
       })
-      .flatMap(response -> {
-        assertThat(response.statusCode()).isEqualTo(400);
-        assertThat(response.statusMessage()).isEqualTo("Bad Request");
-        return response.rxBody();
-      })
-      .toFlowable()
-      .test();
-
-    obs.await(30000, TimeUnit.MILLISECONDS);
-    obs
-      .assertComplete()
-      .assertValue(buffer -> {
-        assertThat(buffer.toString()).isEqualTo(
-          "An other message in a bottle... from the anonymous consumer"
-        );
-        return true;
-      })
-      .assertNoErrors();
-
-    wiremock.verify(1, getRequestedFor(urlPathEqualTo("/endpoint")));
+      .flatMapPublisher(InterruptPolicyIntegrationTest::buildResult)
+      .subscribe(
+        asserts(
+          ctx,
+          (softly, result) -> {
+            softly.assertThat(result.statusCode()).isEqualTo(400);
+            softly
+              .assertThat(result.body())
+              .hasToString(
+                "An other message in a bottle... from the anonymous consumer"
+              );
+          },
+          () -> wiremock.verify(1, getRequestedFor(urlPathEqualTo("/endpoint")))
+        ),
+        ctx::failNow
+      );
   }
 
   @SneakyThrows
   @Test
   @DisplayName("Should do interrupt on-response-content")
   @DeployApi("/apis/interrupt-onresponsecontent.json")
-  void shouldDoInterruptOnResponseContent(HttpClient client) {
+  void shouldDoInterruptOnResponseContent(
+    HttpClient client,
+    VertxTestContext ctx
+  ) {
     wiremock.stubFor(post("/endpoint").willReturn(ok("response from backend")));
 
-    final var obs = client
+    client
       .rxRequest(HttpMethod.POST, "/test")
-      .flatMap(request -> request.rxSend())
-      .flatMap(response -> {
-        assertThat(response.statusCode()).isEqualTo(500);
-        assertThat(response.statusMessage()).isEqualTo("Internal Server Error");
-        return response.rxBody();
-      })
-      .toFlowable()
-      .test();
-
-    obs.await(30000, TimeUnit.MILLISECONDS);
-    obs
-      .assertComplete()
-      .assertValue(buffer -> {
-        assertThat(buffer.toString()).isEqualTo("Message in a bottle...");
-        return true;
-      })
-      .assertNoErrors();
-
-    wiremock.verify(1, postRequestedFor(urlPathEqualTo("/endpoint")));
+      .flatMap(HttpClientRequest::rxSend)
+      .flatMapPublisher(InterruptPolicyIntegrationTest::buildResult)
+      .subscribe(
+        asserts(
+          ctx,
+          (softly, result) -> {
+            softly.assertThat(result.statusCode()).isEqualTo(500);
+            softly
+              .assertThat(result.body())
+              .hasToString("Message in a bottle...");
+          },
+          () ->
+            wiremock.verify(1, postRequestedFor(urlPathEqualTo("/endpoint")))
+        ),
+        ctx::failNow
+      );
   }
 
   @SneakyThrows
@@ -288,36 +284,58 @@ class InterruptPolicyIntegrationTest
   @DisplayName("Should do interrupt on-response-content with response template")
   @DeployApi("/apis/interrupt-onresponsecontent-responsetemplate.json")
   void shouldDoInterruptOnResponseContentWithResponseTemplate(
-    HttpClient client
+    HttpClient client,
+    VertxTestContext ctx
   ) {
     wiremock.stubFor(post("/endpoint").willReturn(ok("response from backend")));
 
-    final var obs = client
+    client
       .rxRequest(HttpMethod.POST, "/test")
-      .flatMap(request -> {
-        request.putHeader("my-custom-header", "the anonymous consumer");
-        return request.rxSend(
-          io.vertx.rxjava3.core.buffer.Buffer.buffer("request payload")
-        );
-      })
-      .flatMapPublisher(response -> {
-        assertThat(response.statusCode()).isEqualTo(400);
-        assertThat(response.statusMessage()).isEqualTo("Bad Request");
-        return response.toFlowable();
-      })
-      .test();
-
-    obs.await(30000, TimeUnit.MILLISECONDS);
-    obs
-      .assertComplete()
-      .assertValue(buffer -> {
-        assertThat(buffer.toString()).isEqualTo(
-          "An other message in a bottle... from the anonymous consumer"
-        );
-        return true;
-      })
-      .assertNoErrors();
-
-    wiremock.verify(1, postRequestedFor(urlPathEqualTo("/endpoint")));
+      .flatMap(request ->
+        request
+          .putHeader("my-custom-header", "the anonymous consumer")
+          .rxSend(Buffer.buffer("request payload"))
+      )
+      .flatMapPublisher(InterruptPolicyIntegrationTest::buildResult)
+      .subscribe(
+        asserts(
+          ctx,
+          (softly, result) -> {
+            softly.assertThat(result.statusCode()).isEqualTo(400);
+            softly
+              .assertThat(result.body())
+              .hasToString(
+                "An other message in a bottle... from the anonymous consumer"
+              );
+          },
+          () ->
+            wiremock.verify(1, postRequestedFor(urlPathEqualTo("/endpoint")))
+        ),
+        ctx::failNow
+      );
   }
+
+  private static @NonNull Flowable<Result> buildResult(
+    HttpClientResponse response
+  ) {
+    return response
+      .toFlowable()
+      .map(buffer -> new Result(response.statusCode(), buffer));
+  }
+
+  private static Consumer<Result> asserts(
+    VertxTestContext ctx,
+    BiConsumer<SoftAssertions, Result> ass,
+    Runnable verify
+  ) {
+    return result ->
+      ctx
+        .verify(() -> {
+          assertSoftly(softAssertions -> ass.accept(softAssertions, result));
+          verify.run();
+        })
+        .completeNow();
+  }
+
+  record Result(int statusCode, Buffer body) {}
 }
